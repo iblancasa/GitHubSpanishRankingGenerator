8fbda373b9c770dd6d164f6f23ec488007443024
Israel Blancas <iblancasa@gmail.com>
2016-02-03 18:21:22 +0100

    Changing process.py to process a city from a config file

--- process.py
+++ process.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python
+
 import os
 import sys
 from githubcity import *
 
+def main(argv):
+    idGH = os.environ.get('GH_ID')
+    secretGH = os.environ.get('GH_SECRET')
+    ciudad = GitHubCity(idGH, secretGH,debug=True)
+    ciudad.readConfigFromJSON(argv[0])
+    ciudad.getCityUsers()
+    ciudad.export(os.path.abspath("html/city-template.html"),os.path.abspath("generated/asd.html"), "contributions")
 
-idGH = os.environ.get('GH_ID')
-secretGH = os.environ.get('GH_SECRET')
-locations = ["Algeciras",  "Arcos De La Frontera", "Barbate", "Los Barrios", "Cadiz", "Cádiz",  "Chiclana", "Chiclana De La Frontera", "Chipiona", "Conil De La Frontera",  "Jerez", "Jeréz", "Jerez De La Frontera", "Jimena De La Frontera", "La Línea", "Medina-Sidonia",   "Puerto De Santa María", "El Puerto De Santa María", "Puerto Real","Rota", "San Roque", "Sanlúcar", "Sanlúcar De Barrameda", "Setenil","Tarifa",  "Ubrique", "Vejer", "Vejer De La Frontera", "Villaluenga","Villamartín"]
-ciudad = GitHubCity(idGH, secretGH, city="Cádiz", locations=locations,debug=True)
-ciudad.calculateBestIntervals()
-ciudad.configToJson("configuration/andalucia/cadiz.json")
+if __name__ == "__main__":
+   main(sys.argv[1:])
