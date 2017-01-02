563ca8815ac465915dbfab7a5e3487c701729ff4
Israel Blancas <iblancasa@gmail.com>
2017-01-02 21:32:27 +0100

    Some changes to new format

--- process.py
+++ process.py
@@ -2,15 +2,24 @@
 
 import os
 import sys
+import datetime
 from githubcity import *
 
 def main(argv):
     idGH = os.environ.get('GH_ID')
     secretGH = os.environ.get('GH_SECRET')
-    ciudad = GitHubCity(idGH, secretGH,debug=True)
+    ciudad = GitHubCity(idGH, secretGH)
     ciudad.readConfigFromJSON(argv[0])
     ciudad.getCityUsers()
-    ciudad.export(os.path.abspath("jsontemplate/template"),os.path.abspath(argv[1]+".json"), "contributions")
+
+    extraData = {
+        "date": datetime.datetime.now().strftime("%I:%M%p-%B-%d-%Y"),
+        #"city": ciudad._city
+    }
+
+    ciudad.export(os.path.abspath("jsontemplate/template"),os.path.abspath(argv[1]+".json"), "public", data=extraData)
+    #ciudad.export(os.path.abspath("jsontemplate/template2"),os.path.abspath(argv[1]+".json"), "contributions", data=extraData)
+
 
 if __name__ == "__main__":
    main(sys.argv[1:])
