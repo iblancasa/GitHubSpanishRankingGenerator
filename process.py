7efdec09489b065d6b81837f7aca12170d5a8fa6
Israel Blancas <iblancasa@gmail.com>
2017-08-21 22:03:27 +0200

    Add filters

--- process.py
+++ process.py
@@ -3,23 +3,25 @@
 import os
 import sys
 import datetime
-from githubcity import *
+from githubcity.ghcity import *
 
 def main(argv):
     idGH = os.environ.get('GH_ID')
     secretGH = os.environ.get('GH_SECRET')
     ciudad = GitHubCity(idGH, secretGH)
     ciudad.readConfigFromJSON(argv[0])
-    ciudad.getCityUsers()
-
     extraData = {
         "date": datetime.datetime.now().strftime("%I:%M%p-%B-%d-%Y"),
-        "city": ciudad._city
+        "city": ciudad.city
     }
+    ciudad.calculateBestIntervals()
+    ciudad.addFilter("repos", ">1")
+    ciudad.addFilter("followers", ">1")
+    ciudad.getCityUsers()
 
     ciudad.export(os.path.abspath("jsontemplate/template"),os.path.abspath(argv[1]+".json"), "public", data=extraData)
     ciudad.export(os.path.abspath("jsontemplate/template2"),os.path.abspath(argv[1]+".md"), "public", data=extraData)
-
+    ciudad.configToJson(os.path.abspath(argv[0]))
 
 if __name__ == "__main__":
    main(sys.argv[1:])
